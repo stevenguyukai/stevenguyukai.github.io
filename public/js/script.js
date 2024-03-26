81605ee5aa50e5c1a53109e5d50d34d4da9c4a34
stevenguyukai <stevenguyukai@users.noreply.github.com>
2024-03-26 10:10:26 +0000

    deploy: e071303f48e2d891452a23c85db5204ec617bf34

--- public/js/script.js
+++ public/js/script.js
@@ -5,107 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
 	wholeContentDiv.addEventListener('scroll', function() {
 
 		// Scrolled: [210, 420]
-		const vh = wholeContentDiv.scrollHeight / 420;
+		const vh = wholeContentDiv.scrollHeight / 630;
 		const scrolled = (wholeContentDiv.scrollTop + wholeContentDiv.clientHeight) / vh;
 		if(scrolled >= 210 && scrolled <= 420){
-			// document.getElementById("wholeContent").style.setProperty("--progress", (scrolled - 210) / 210)
+			// This is the range from first page to second page
+			// while scrolling, the animation will be triggered
+			document.getElementById("educationBox").style.setProperty("--progress", (scrolled - 210) / 210)
+			document.getElementById("majorBox").style.setProperty("--progress", (scrolled - 210) / 210)
 		}
 	});
 });
-
-
-
-// whole.addEventListener('scroll', function() {
-// 	console.log('Div is being scrolled');
-// });
-
-// window.addEventListener("scroll", (e) => {
-	// const span_i = document.querySelectorAll('span i');
-	// const area2 = document.querySelectorAll(".secondText");
-	// const area3 = document.querySelectorAll(".thirdText");
-	// const area4 = document.querySelectorAll(".forthText");
-
-	// const vh = html.scrollHeight / 420;
-	// const scrolled = (html.scrollTop + html.clientHeight) / vh;
-
-	// console.log(e);
-
-	// if (scrolled < 200) {
-	//
-	// 	// 第一个100vh元素
-	// 	for (var i = 0; i < span_i.length; i++) {
-	// 		span_i[i].style.setProperty("--progress", (scrolled - 100) / 100)
-	// 	}
-	// 	for (var i = 0; i < mark.length; i++) {
-	// 		if (i == 2 || i == 3 || i == 4) {
-	// 			continue
-	// 		} else {
-	// 			mark[i].style.setProperty("--progress", (scrolled - 90) / 100)
-	// 		}
-	// 	}
-	// 	document.getElementById("introPic").style.setProperty("--progress", (scrolled - 100) / 100)
-	// 	document.getElementById("clubActivityPic").style.setProperty("--progress", (scrolled - 200) / 100)
-	// 	document.getElementById("clubActivityShow").style.setProperty("--progress", (scrolled - 200) / 100)
-	//
-	// } else if (scrolled >= 200 && scrolled < 300) {
-	// 	// 第二个100vh元素
-	// 	for (var i = 0; i < area3.length; i++) {
-	// 		area3[i].style.setProperty("--progress", (scrolled - 200) / 100)
-	// 	}
-	// 	for (var i = 0; i < 3; i++) {
-	// 		document.getElementsByClassName("newKnowledgeCard")[i].style.setProperty("--progress", (scrolled -
-	// 			200) / 100)
-	// 	}
-	// 	if (scrolled >= 260) {
-	// 		for (var i = 0; i < 2; i++) {
-	// 			document.getElementsByClassName("brainGameItem")[i].style.color = "white"
-	// 			document.getElementsByClassName("newKnowledgeItem")[i].style.color = "#FF7818"
-	// 			document.getElementsByClassName("logItem")[i].style.color = "white"
-	// 		}
-	// 	} else {
-	// 		for (var i = 0; i < 2; i++) {
-	// 			document.getElementsByClassName("newKnowledgeItem")[i].style.color = "white"
-	// 		}
-	// 	}
-	//
-	// } else if (scrolled >= 300 && scrolled < 400) {
-	// 	// 第三个100vh元素
-	// 	hideQrCode()
-	// 	if (scrolled >= 320 && scrolled <= 380) {
-	// 		for (var i = 0; i < area4.length; i++) {
-	// 			area4[i].style.setProperty("--progress", (scrolled - 320) / 60)
-	// 		}
-	// 	}
-	//
-	// 	for (var i = 0; i < mark.length; i++) {
-	// 		if (i == 0 || i == 1 || i == 2) {
-	// 			continue
-	// 		} else {
-	// 			mark[i].style.setProperty("--progress", (scrolled - 300) / 100)
-	// 		}
-	// 	}
-	// 	if (scrolled >= 360) {
-	// 		for (var i = 0; i < 2; i++) {
-	// 			document.getElementsByClassName("brainGameItem")[i].style.color = "white"
-	// 			document.getElementsByClassName("newKnowledgeItem")[i].style.color = "white"
-	// 			document.getElementsByClassName("logItem")[i].style.color = "#FF7818"
-	// 		}
-	// 	} else {
-	// 		for (var i = 0; i < 2; i++) {
-	// 			document.getElementsByClassName("logItem")[i].style.color = "white"
-	// 		}
-	// 	}
-	//
-	// } else if (scrolled >= 410) {
-	// 	// 第四个100vh元素
-	// 	for (var i = 0; i < 2; i++) {
-	// 		document.getElementsByClassName("brainGameItem")[i].style.color = "white"
-	// 		document.getElementsByClassName("newKnowledgeItem")[i].style.color = "white"
-	// 		document.getElementsByClassName("logItem")[i].style.color = "white"
-	// 	}
-	// }
-
-// })
 
 
 function fadeElement(elementId, duration, fadeOut) {
@@ -191,15 +100,30 @@ function homeBtn(){
 	smoothScrollTo(wholeContentDiv, vhToPixels(0), 1000);
 }
 function projectsBtn(){
-	alert('🚧 Under construction!');
+	smoothScrollTo(wholeContentDiv, vhToPixels(200), 1000);
 }
 function educationBtn(){
 	smoothScrollTo(wholeContentDiv, vhToPixels(100), 1000);
 }
-function skillsBtn(){
+function courseBtn(){
 	alert('🚧 Under construction!');
 }
 function contactBtn(){
 	alert('🚧 Under construction!');
 }
 
+function directToProject1(){
+	window.open("https://antalmanac.com", "_blank");
+}
+
+function directToProject2(){
+	window.open("https://github.com/stevenguyukai/IEEE-MicroMouse", "_blank");
+}
+
+function directToProject3(){
+	window.open("http://stevenguyukai.github.io/PRO_Website", "_blank");
+}
+
+function directToProject4(){
+	window.open("https://github.com/stevenguyukai/IR24W-A3-G42", "_blank");
+}
